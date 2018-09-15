7b04aebc110212bfbffeac2f8dc2dcd86a75cd9e
ezequiel454 <ezequiel454@gmail.com>
2018-09-15 16:08:33 -0300

    Refactor binaryGap

--- codility/binaryGap.js
+++ codility/binaryGap.js
@@ -1,36 +1,52 @@
 const binario = (decimalParam) => {
   let decimal = decimalParam
   let lista = []
-  while(decimal != 0){
+  while(decimal !== 0){
     lista.unshift(decimal%2)
     decimal = Math.floor(decimal/2)
   }
   return lista
 }
 
-function solution(n){
-  const lista = binario(n)
-  let soma = 0
-  let contador = 0
-  let cond = false
-  for (let index = 0; index < lista.length; index++) {
-    if(lista[index] === 1 && cond === false){
-      cond = true
-    }else if(lista[index] === 1 && cond === true){
-      if (soma -1 < contador){
-        soma = contador
+const solution = (N) => {
+  let listaBinaria = binario(N)
+  let resposta = 0
+  let condAchou1 = false
+  let somaTemp = 0
+  listaBinaria.forEach((e, index)=> {
+    if(e === 1 && !condAchou1){
+      condAchou1 = true
+    }else if(e === 1 && condAchou1){
+      if(listaBinaria[index-1] !== 1){
+        resposta = resposta < somaTemp ? somaTemp : resposta 
+        somaTemp = 0
       }
-      contador = 0
+    }else if(e === 0 && condAchou1){
+      somaTemp++
     }
-    if(cond === true){
-      contador++
-    }
-  }
-  if(soma === 0){
-    return 0
-  }else{
-    return soma - 1
-  }
+  })
+  return resposta
 }
+
 //console.log(solution(529)) //deve retornar 4
 //console.log(solution(1041)) //deve retornar 5
+
+
+/*
+Task description
+A binary gap within a positive integer N is any maximal sequence of consecutive zeros that is surrounded by ones at both ends in the binary representation of N.
+
+For example, number 9 has binary representation 1001 and contains a binary gap of length 2. The number 529 has binary representation 1000010001 and contains two binary gaps: one of length 4 and one of length 3. The number 20 has binary representation 10100 and contains one binary gap of length 1. The number 15 has binary representation 1111 and has no binary gaps. The number 32 has binary representation 100000 and has no binary gaps.
+
+Write a function:
+
+function solution(N);
+
+that, given a positive integer N, returns the length of its longest binary gap. The function should return 0 if N doesn't contain a binary gap.
+
+For example, given N = 1041 the function should return 5, because N has binary representation 10000010001 and so its longest binary gap is of length 5. Given N = 32 the function should return 0, because N has binary representation '100000' and thus no binary gaps.
+
+Write an efficient algorithm for the following assumptions:
+
+N is an integer within the range [1..2,147,483,647].
+Copyright 2009–2018 by Codility Limited. All Rights Reserved. Unauthorized copying, publication or disclosure prohibited.
